da2573e72c3ed7e14911469cf687f5ddc47fb5e3
lgupta <lgupta@bol.com>
2021-04-05 21:19:42 +0200

    Delete exception

--- src/main/java/com/bol/gameservice/exception/GameValidationException.java
+++
@@ -1,9 +0,0 @@
-package com.bol.gameservice.exception;
-
-public class GameValidationException extends Exception {
-
-    public GameValidationException(String message) {
-        super(message);
-    }
-    
-}
