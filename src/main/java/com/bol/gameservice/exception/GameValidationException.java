5b7c0ff3e56add752fff79c10674bbdde8be364f
lgupta <lgupta@bol.com>
2021-04-05 20:54:23 +0200

    Refactor exception

---
+++ src/main/java/com/bol/gameservice/exception/GameValidationException.java
@@ -0,0 +1,9 @@
+package com.bol.gameservice.exception;
+
+public class GameValidationException extends Exception {
+
+    public GameValidationException(String message) {
+        super(message);
+    }
+    
+}
